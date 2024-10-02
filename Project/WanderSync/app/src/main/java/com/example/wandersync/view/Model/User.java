41bbd075a5694965793b098506385e97b8740a1f
Cabaretman <rafay11hadi@gmail.com>
2024-10-01 23:11:00 -0400

    Added logic and firebase stuff for create account

---
+++ Project/WanderSync/app/src/main/java/com/example/wandersync/view/Model/User.java
@@ -0,0 +1,16 @@
+package com.example.wandersync.view.Model;
+
+public class User {
+    public String username;
+    public String email;
+    public String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {}
+}
+
