29ee8275bd84585c0eac0bd99074487d6a89bed4
saiplusplus <hellosaibalaji@gmail.com>
2024-10-04 13:29:51 -0400

    updated my file structure

--- Project/WanderSync/app/src/main/java/com/example/wandersync/view/MainActivity.java
+++ Project/WanderSync/app/src/main/java/com/example/wandersync/view/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.wandersync.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button quitButton = findViewById(R.id.buttonQuit);
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
+
+
 }
