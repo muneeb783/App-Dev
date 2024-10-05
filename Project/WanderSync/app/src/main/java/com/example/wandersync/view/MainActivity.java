4486420cdfdbe618fee7c3e8565915383d5266e6
saiplusplus <hellosaibalaji@gmail.com>
2024-10-04 22:41:30 -0400

    Updated Welcome Page with Get Started and Quit functionality and designing

--- Project/WanderSync/app/src/main/java/com/example/wandersync/view/MainActivity.java
+++ Project/WanderSync/app/src/main/java/com/example/wandersync/view/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.wandersync.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +15,28 @@ import com.example.wandersync.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonStart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        buttonStart = findViewById(R.id.button_start);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
