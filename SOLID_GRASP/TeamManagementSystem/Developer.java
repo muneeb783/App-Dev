367e54f5b77ac94809a86ec3e696c04d98134b84
Cabaretman <rafay11hadi@gmail.com>
2024-10-20 22:51:51 -0400

    fixed everything 3

--- SOLID_GRASP/TeamManagementSystem/Developer.java
+++ SOLID_GRASP/TeamManagementSystem/Developer.java
@@ -1,16 +1,18 @@
 import java.util.logging.Logger;
+
 public class Developer implements IDeveloper {
 
     private String name;
     private String email;
     private static final Logger logger = Logger.getLogger(TeamManager.class.getName());
+
     public Developer(String name, String email) {
         this.name = name;
         this.email = email;
     }
 
     @Override
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
@@ -30,9 +32,10 @@ public class Developer implements IDeveloper {
 
     @Override
     public void receiveTask(Task task) {
-        logger.info(String.format("%s received task: %s", name, task.getTitle()));
+        if (logger.isLoggable(Level.INFO)) {
+            logger.info(String.format("%s received task: %s", name, task.getTitle()));
+        }
     }
-
     @Override
     public void completeTask(Task task) {
         task.execute();
