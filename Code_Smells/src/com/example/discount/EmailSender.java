e8f7faf621eb02aebed0997c8e578be99beb3b0d
ArishV1 <arish.virani@gmail.com>
2024-11-03 00:51:43 -0400

    More editing of code refactor in Code_Smell

--- Code_Smells/src/com/example/discount/EmailSender.java
+++ Code_Smells/src/com/example/discount/EmailSender.java
@@ -5,9 +5,15 @@ import java.util.logging.Logger;
 public class EmailSender {
     private static final Logger logger = Logger.getLogger(EmailSender.class.getName());
 
+    private EmailSender() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
     public static void sendEmail(String customerEmail, String subject, String message) {
-        logger.info(String.format("Email to: %s", customerEmail));
-        logger.info(String.format("Subject: %s", subject));
-        logger.info(String.format("Body: %s", message));
+        if (logger.isLoggable(java.util.logging.Level.INFO)) {
+            logger.info(String.format("Email to: %s", customerEmail));
+            logger.info(String.format("Subject: %s", subject));
+            logger.info(String.format("Body: %s", message));
+        }
     }
 }
--- Code_Smells/src/com/example/discount/OrderNotification.java
+++ Code_Smells/src/com/example/discount/OrderNotification.java
@@ -1,14 +1,16 @@
 package com.example.discount;
 
-import java.util.logging.Logger;
-
 public class OrderNotification {
+    private OrderNotification() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
     public static void sendConfirmationEmail(Order order) {
         StringBuilder messageBuilder = new StringBuilder();
-        messageBuilder.append(String.format("Thank you for your order, %s!\n\nYour order details:\n", order.getCustomerName()));
+        messageBuilder.append(String.format("Thank you for your order, %s!%n%nYour order details:%n", order.getCustomerName()));
         
         for (Item item : order.getItems()) {
-            messageBuilder.append(String.format("%s - $%.2f\n", item.getName(), item.getPrice()));
+            messageBuilder.append(String.format("%s - $%.2f%n", item.getName(), item.getPrice()));
         }
         
         messageBuilder.append(String.format("Total: $%.2f", order.calculateTotalPrice()));
