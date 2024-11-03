71a399583592ecff118a757858fdb2507a7c47f1
ArishV1 <arish.virani@gmail.com>
2024-11-02 23:44:00 -0400

    Added refactored code for Code_Smaells

---
+++ Code_Smells/src/AmountDiscount.java
@@ -0,0 +1,12 @@
+public class AmountDiscount implements Discount {
+    private final double amount;
+
+    public AmountDiscount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public double apply(double price) {
+        return price - amount;
+    }
+}
---
+++ Code_Smells/src/TaxableItem.java
@@ -0,0 +1,24 @@
+public class TaxableItem extends Item {
+    private double taxRate;
+
+    public TaxableItem(String name, double price, int quantity, Discount discount, double taxRate) {
+        super(name, price, quantity, discount);
+        this.taxRate = taxRate;
+    }
+
+    public double getTaxRate() {
+        return taxRate;
+    }
+
+    public void setTaxRate(double rate) {
+        if (rate >= 0) {
+            taxRate = rate;
+        }
+    }
+
+    @Override
+    public double getTotalPrice() {
+        double discountedPrice = super.getTotalPrice();
+        return discountedPrice + (discountedPrice * taxRate / 100);
+    }
+}
